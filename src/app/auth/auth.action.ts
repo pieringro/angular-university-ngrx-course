cc94674e3a1b11b6a811549260c1214366c634ba
Pierluigi Ingrosso <pierluigi.ingrosso@mtmprojectsrlmonopoli.onmicrosoft.com>
2020-10-16 08:42:07 +0200

    section 3 lesson 13

--- src/app/auth/auth.action.ts
+++ src/app/auth/auth.action.ts
@@ -2,8 +2,13 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from './model/user.model';
 
+// this is an action creator
+// you have to call login({user}) to create an action
 export const login = createAction(
     "[Login Page] User Login",
     props<{ user: User }>()
 );
 
+export const logout = createAction(
+    "[Top Menu] Logout"
+);
