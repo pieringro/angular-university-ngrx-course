fa044b10b6a4eae0875cd33cc844489c84ac6865
pierprogramm <pierprogramm@gmail.com>
2020-10-17 18:27:28 +0200

    section 4 lesson 24 - Metareducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -17,4 +17,13 @@ export const reducers: ActionReducerMap<AppState> = {
 };
 
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log("state before:", state)
+    console.log("action:", action);
+    return reducer(state, action);
+  };
+}
+
+export const metaReducers: MetaReducer<AppState>[] =
+  !environment.production ? [logger] : [];
