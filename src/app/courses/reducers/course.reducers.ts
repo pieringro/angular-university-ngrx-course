4c8d3fc6b22013b008dd521e43419992a8678af3
pierprogramm <pierprogramm@gmail.com>
2020-10-18 20:29:48 +0200

    section 5 lesson 30 - Reducer for Entity with Entity Adapter

---
+++ src/app/courses/reducers/course.reducers.ts
@@ -0,0 +1,32 @@
+import { Course } from '../model/course';
+import { createEntityAdapter, EntityState } from '@ngrx/entity';
+import {
+    ActionReducer,
+    ActionReducerMap,
+    createFeatureSelector,
+    createReducer,
+    createSelector,
+    MetaReducer,
+    on
+} from '@ngrx/store';
+import { CourseActions } from "../action-types";
+
+export interface CoursesState extends EntityState<Course> {
+
+    // entity format (from interface EntityState extended)
+    // entities: { [key: number]: Course },
+    // ids: number[],
+
+}
+
+export const adapter = createEntityAdapter<Course>();
+
+export const initialCoursesState = adapter.getInitialState();
+
+
+export const coursesReducer = createReducer(
+    initialCoursesState,
+
+    on(CourseActions.allCoursesLoaded,
+        (state, action) => adapter.addAll(action.courses, state))
+);
